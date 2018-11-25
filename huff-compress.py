1bc3736c5bcce08fae78bf97fbed587167051945
Yang-Xavier <xavier_ybx@163.com>
2018-11-25 15:36:01 +0000

    finished decompression and compression need to be optimized

--- huff-compress.py
+++ huff-compress.py
@@ -1,14 +1,9 @@
-import argparse
-import array
-import re
-import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-s', type=str, help='Symbol')
-parser.add_argument('input', type=str, help="Input file")
-args = parser.parse_args()
+import array, argparse, re,time, pickle
 
-def build_huff(terms):
+start = time.clock()
+
+def build_huffman(terms):
     result = {}
     def iterate(code, tree):
         if isinstance(tree,str):
@@ -16,28 +11,39 @@ def build_huff(terms):
             return
         iterate(code+'0',tree['0'])
         iterate(code+'1',tree['1'])
-
-    while (len(terms) != 1) :
-        min1 = terms[0]
-        min2 = terms[1]
-        terms.append(({'0':min1[0],'1':min2[0]}, min1[1]+min2[1]))
-        del terms[0]
-        del terms[0]
-        terms = sorted(terms, key=lambda x:x[1])
+    while (len(terms) != 1):
+        while (len(terms) != 1) :
+            min1 = terms[0]
+            min2 = terms[1]
+            terms.insert(2,({'0':min1[0],'1':min2[0]}, min1[1]+min2[1]))
+            del terms[0]
+            del terms[0]
+            terms = sorted(terms, key=lambda x:x[1])
     iterate('',terms[0][0])
-    return result
+    return result, terms[0][0]
+
 
 def convert_binary(huff_str):
     data = array.array('B')
+    c_b = '00000000'
+    length = len(huff_str)
+    huff_str += c_b[:(8-len(huff_str)%8)]
     for i in range(0,len(huff_str), 8):
-        data.append(int(huff_str[i:i+8],2))
-    return data
+        bytes = int(huff_str[i:i+8],2)
+        data.append(bytes)
+    # print(int(huff_str[:8],2))   238
+    print(huff_str[-8:])
+    return data,length
 
-def store(file_path,huff_str):
-    data = convert_binary(huff_str)
-    with open(file_path+".test","wb") as f:
-             data.tofile(f)
 
+def store(file_path,huff_str, model):
+    data, length = convert_binary(huff_str)
+    with open(file_path+".bin","wb") as f:
+        data.tofile(f)
+    with open(file_path+'.plk', "wb") as f:
+        model['length'] = length
+        print(length)
+        pickle.dump(model,f)
     return
 
 def format_data(init_data):
@@ -51,39 +57,55 @@ def format_data(init_data):
 
 
 # ________main__________
+# parser = argparse.ArgumentParser()
+# parser.add_argument('-s', type=str, help='Symbol')
+# parser.add_argument('input', type=str, help="Input file")
+# args = parser.parse_args()
 
-if "s" in args and "input" in args:
-    symbol = args.s
-    file_path  = args.input
-    text = ""
+# if "s" in args and "input" in args:
+#     symbol = args.s if args.s else 'word'
+#     file_path  = args.input if args.input else 'infile.txt'
+#     text = ""
+#     else:
+#         sys.exit()
 
-    with open(file_path) as f:
-        text = f.read()
-    if symbol == "char":
-        term_array = format_data(text)
-    if symbol == 'word':
-        word_pattern = re.compile(r'([a-z]+)', re.I)
-        symbol_pattern = re.compile(r'[^a-z]{1}',re.I)
-        word_r = word_pattern.findall(text)
-        symbol_r = symbol_pattern.findall(text)
-        word_r.extend(symbol_r)
-        term_array = format_data(word_r)
+symbol = 'word'
+file_path  =  'infile.txt'
+text = ""
 
-    term_array = sorted(term_array.items(), key=lambda c: c[1])
-    terms_huff=build_huff(term_array)
+with open(file_path) as f:
+    text = f.read()
+term_array = None
+# read content
+if symbol == "char":
+    term_array = format_data(text)
+if symbol == 'word':
+    word_pattern = re.compile(r'([a-z]+)', re.I)
+    symbol_pattern = re.compile(r'[^a-z]{1}',re.I)
+    word_r = word_pattern.findall(text)     #   find all word
+    symbol_r = symbol_pattern.findall(text)     # find all the symbol include the invisible character
+    term_array = format_data(word_r+symbol_r)
+# term_array is the frequency of each term in the text
+term_array = sorted(term_array.items(), key=lambda c: c[1])
+terms_huffman, huffman_model =build_huffman(term_array)
 
-    huff_str = ""
-    if symbol == "char":
-        for c in text:
-            huff_str+=terms_huff[c]
-    store(file_path,huff_str)
-    # print(terms_huff)
-    # compress(args.s,args.input)
-    # terms, text = init_data(symbol ,file_path)
-    # terms_huff = build_huff(terms)
-    # huff_str = ""
-    # for c in text:
-    #     huff_str+=terms_huff[c]
-    # store(file_path,huff_str)
-else:
-    sys.exit()
+huff_str = ""
+if symbol == "char":
+    for c in text:
+        huff_str+=terms_huffman[c]
+# it is easy for character
+if symbol == "word":
+    term = ''
+    word_pattern = re.compile(r'([a-z])', re.I)
+    for c in text:      # char by char reading the text
+        if word_pattern.match(c):
+            term+=c
+        else:
+            if len(term)!=0:       # if the term has the punctuation or invisible character
+                huff_str+=(terms_huffman[term]+terms_huffman[c])
+                term = ''
+            else:
+                huff_str += terms_huffman[c]
+# print(len(huff_str)) #5522874
+store(file_path, huff_str, huffman_model)
+print('%.2f'%(time.clock()-start))
--- huff-decompress.py
+++ huff-decompress.py
@@ -1,10 +1,62 @@
-import sys
-import getopt
-import argparse
+import array, argparse,time, pickle,sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-s', type=str, help='Symbol')
-parser.add_argument('input', type=str, help="Input file")
-args = parser.parse_args()
 
-print(args)
+class Huffman_model:
+    def __init__(self, model):
+        self.model = model
+        self.iterate_model = model
+
+    def iterate_(self, index):
+        node = self.iterate_model[index]
+        if isinstance(node, str):
+            return node
+        else:
+            self.iterate_model = node
+
+    def reset(self):
+        self.iterate_model = self.model
+
+#---------------main----------------------------
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument('bin', type=str, help="the compression file XXXX.bin")
+# args = parser.parse_args()
+#
+# if 'bin' in args:
+#     bin_file = args.bin
+#     plk_file = bin_file[:bin_file.find('.bin')] + '.plk'
+# else:
+#     sys.exit()
+bin_file = 'infile.txt.bin'
+plk_file = bin_file[:bin_file.find('.bin')] + '.plk'
+out_file = bin_file[:bin_file.find('.bin')] + '.out'
+length = 0
+model = {}
+with open(plk_file, 'rb') as f:
+    model = pickle.load(f)
+length = model['length']
+huffman = Huffman_model(model)
+
+bytes = bytes();
+data = array.array('B')
+with open(bin_file, 'rb') as f:
+    bytes = f.read()
+
+length_r = 0
+c_b = '00000000'
+recover_file = ''
+for ab in bytes:
+    bstr = bin(ab)[2:]
+    bstr = c_b[0:(8-len(bstr))] + bstr
+    for b in bstr:
+        r = huffman.iterate_(b)
+        length_r+=1
+        if isinstance(r,str):
+            recover_file += r
+            huffman.reset()
+        if length_r >=length:
+            break
+print(bin(bytes[-1]))
+with open(out_file, 'w') as f:
+    f.write(recover_file)
+# print(l) # 4955339 #5522880 5522874
